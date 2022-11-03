0ec503c6341e63568668afcc262c7188d71c663e
bireweich <yasmine.douar@uzh.ch>
2022-11-03 11:09:39 +0100

    knn neighbours

--- ML_models/decision_tree_titanic.jl
+++ ML_models/decision_tree_titanic.jl
@@ -1,4 +1,5 @@
 using ScikitLearn
+using DataFrames
 import ScikitLearn: fit!, predict
 import ScikitLearn: plot_tree
 @sk_import tree: DecisionTreeClassifier
@@ -9,9 +10,9 @@ using GraphViz
 import GraphViz: Source
 
 using CSV
-X_test = CSV.read("features/titanic_test.csv", DataFrame)
+X_test = CSV.read("../features/titanic_test.csv", DataFrame)
 
-include("features/feature_formatting_titanic.jl")
+include("../features/feature_formatting_titanic.jl")
 
 tit
 
@@ -41,4 +42,6 @@ CSV.write("submission_decision_tree.csv", submission)
 dot_data = export_graphviz(clf) 
 graph = GraphViz.Source(dot_data) 
 graph.render("iris")  =#
+
+
 clf.fit(X, Y)
