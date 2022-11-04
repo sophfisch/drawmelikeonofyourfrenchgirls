0439a31a3e99bcf87daed6ec525241cd29764875
lauranieba <115983357+lauranieba@users.noreply.github.com>
2022-11-04 09:04:39 +0100

    Add files via upload

--- ML_models/random_forest.jl
+++ ML_models/random_forest.jl
@@ -6,10 +6,14 @@ using ScikitLearn
 tit_train = CSV.read("titanic_train.csv", DataFrame)
 tit_test = CSV.read("titanic_test.csv", DataFrame)
 
+
 Y_train = tit_train[:,1]
 X_train = Matrix(select(tit_train, Not(:Survived)))
 
 X_test =  Matrix(tit_test)
+
+us = CSV.read("us.csv", DataFrame)
+us_m = Matrix(us)
 
 random_forest = RandomForestClassifier(n_estimators=800)
 random_forest.fit(X_train, Y_train)
@@ -39,44 +43,31 @@ kf = StratifiedKFold(n_splits=nsplits, shuffle=true)
 gridsearch = GridSearchCV(model, parameters, scoring="accuracy", cv=kf, n_jobs=2, verbose=0)
 
 # train the model
-fit!(gridsearch,X_train,Y_train)
+gridsearch.fit(X_train,Y_train)
 best_estimator = gridsearch.best_estimator_
 
 
 
 #hyperparameter tuning
-param =  Dict("max_depth" => 3:100, "n_estimators" => 10:10:1000)
-kf = StratifiedKFold(n_splits=nsplits, shuffle=true)
+param =  Dict("max_depth" => 3:5:100, "n_estimators" => 10:10:1000)
+kf = StratifiedKFold(n_splits=5, shuffle=true)
 gridsearch = GridSearchCV(model, param, scoring="accuracy", cv=kf, n_jobs=2, verbose=0)
 
-fit!(gridsearch,X_train,Y_train)
+gridsearch.fit(X_train,Y_train)
 best_estimator = gridsearch.best_estimator_
 
-random_forest_md = RandomForestClassifier(n_estimators=800)
+random_forest_md = RandomForestClassifier(max_depth=13, n_estimators=800)
 random_forest_md.fit(X_train, Y_train)
 
-Y_prediction_md = random_forest.predict(X_test)
+Y_prediction_md = random_forest_md.predict(X_test)
+pre_us = random_forest_md.predict(us_m)
 
 random_forest_md.score(X_train, Y_train)
 
+df_pred_md = DataFrame(PassengerId = test.PassengerId, Survived = Y_prediction_md)
+
+CSV.write("predictions_md.csv", df_pred_md)
 
 
-rf = RandomForestClassifier(n_estimators=100, max_features="auto", oob_score=true, random_state=1, n_jobs=-1)
-clf = GridSearchCV(estimator=rf, param_grid=param, n_jobs=-1)
-clf.fit(X_train, Y_train)
-clf.best_estimator_
+CSV.write("predictions_web.csv", df_pred_web)
 
-
-
-random_forest_web = RandomForestClassifier(n_estimators = 100, max_features = "auto", criterion="gini", min_samples_split=10, min_samples_leaf = 1, n_jobs=-1, oob_score=true, random_state=1)
-random_forest_web.fit(X_train, Y_train)
-
-Y_prediction_web = random_forest_web.predict(X_test)
-
-random_forest_web.score(X_train, Y_train)
-
-
-#feature importance
-importances = DataFrame(feature = ("Survived","Pclass","Sex","Age","Relatives","Fare","Embarked"), importance = random_forest.feature_importances_)
-importances = importances.sort_values("importance",ascending=false).set_index("feature")
-importances.head(15)
